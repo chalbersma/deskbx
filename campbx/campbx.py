d0736772eef72d87f3954eab59e798d4fe234694
Chris Halbersma <chalbersma.12@gmail.com>
2013-04-12 17:01:49 -0500

    GUI going well
    Gui now has last price ask bid along with the top 3 asks and bids on
    campbx. In addition working on integrating historical data.

--- campbx/campbx.py
+++ campbx/campbx.py
@@ -1,18 +1,44 @@
 import json
 import urllib.request
+import logging
+import time
+import datetime
+
 
 class CampBX(object):
+    username = None
+    password = None
+    api_url = "https://campbx.com/api/"
+    #hist_url = "http://bitcoincharts.com/t/trades.csv?symbol=SYMBOL[&start=UNIXTIME][&end=UNIXTIME]"
+    log = None
+
     def __init__(self, master):
-        """Camp BX API Class"""
-        self.username = None
-        self.password = None
-        self.api_url = 'https://campbx.com/api/'
-        self.log = None
-        
+        return
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password    
+    
     def xticker_print(self):
         xtickerurl = self.api_url + "xticker.php"
         request = urllib.request.Request(xtickerurl)
         response = urllib.request.urlopen(request)
         xjson = json.loads((response.read().decode('utf-8')))
         return xjson
+    
+    def xdepth_print(self):
+        xdepthurl = self.api_url + "xdepth.php"
+        request = urllib.request.Request(xdepthurl)
+        response = urllib.request.urlopen(request)
+        xdepth = json.loads((response.read().decode('utf-8')))
+        return xdepth
         
+    def historical(self):
+        now = int(time.time())
+        hourago = now - 60*30
+        xhistoryurl = "http://bitcoincharts.com/t/trades.csv?symbol=CBXUSD&end="+str(hourago)
+        print(xhistoryurl)
+        request = urllib.request.Request(xhistoryurl)
+        response = urllib.request.urlopen(request)
+        print ((response.read().decode('utf-8')))
+        
+        return
--- campbx/gui.py
+++ campbx/gui.py
@@ -23,6 +23,12 @@ class GUI:
         self.biddesc=Label(frame,text="Ask")
         self.biddesc.grid(row=0,column=2)
         
+        #Current Orders Label
+        self.bidsdesc=Label(frame,text="Open Bids")
+        self.bidsdesc.grid(row=4, column=0, columnspan=2)
+        self.asksdesc=Label(frame,text="Open Asks")
+        self.asksdesc.grid(row=4, column=2, columnspan=2)
+        
         #Current Price Label
         self.priceticker=Label(frame,text="Last...")
         self.priceticker.grid(row=1)
@@ -31,11 +37,36 @@ class GUI:
         self.askticker=Label(frame,text="Ask...")
         self.askticker.grid(row=1,column=2)
         
-        self.priceupdate=Button(frame, text="Update", command=self.updateprice())
-        self.priceupdate.grid(row=2)
+        
+        #Top 3 Bids
+        self.bids1=Label(frame,text="$0x0")
+        self.bids1.grid(row=5, column=0, columnspan=2)
+        self.bids2=Label(frame,text="$0x0")
+        self.bids2.grid(row=6, column=0, columnspan=2)
+        self.bids3=Label(frame,text="$0x0")
+        self.bids3.grid(row=7, column=0, columnspan=2)
+        
+        #Top 3 Asks
+        self.asks1=Label(frame,text="$0x0")
+        self.asks1.grid(row=5, column=2, columnspan=2)
+        self.asks2=Label(frame,text="$0x0")
+        self.asks2.grid(row=6, column=2, columnspan=2)
+        self.asks3=Label(frame,text="$0x0")
+        self.asks3.grid(row=7, column=2, columnspan=2)
+        
+        #Update Button (Runs Update)
+        self.priceupdate=Button(frame, text="Update", command=self.update())
+        self.priceupdate.grid(row=2, columnspan=3)
         
         self.note=Label(frame,text="Prices pulled from Campbx")
         self.note.grid(row=3, columnspan=3)
+        
+    def update(self):
+        self.updateprice()
+        self.updateorders()
+        self.updatehistory()
+        return
+        
         
     def updateprice(self):
         connection = CampBX(self)
@@ -50,3 +81,33 @@ class GUI:
         
         
         return
+        
+    def updateorders(self):
+        connection = CampBX(self)
+        orders = connection.xdepth_print()
+        #   "$" + str(orders["Asks"][-2][0]) + " : " + str(orders["Asks"][-2][1]))
+        #   Dollar Sign + Order in $ (rounded two 2) + Volume in Bitcoin (rounded two too
+        #Update Price Tables
+        self.asks1.config(text="$" + str(round(orders["Asks"][-1][0],2)) + " : " + str(round(orders["Asks"][-1][1],2)))
+        self.asks1.update()
+        self.asks2.config(text="$" + str(round(orders["Asks"][-2][0],2)) + " : " + str(round(orders["Asks"][-2][1],2)))
+        self.asks2.update()
+        self.asks3.config(text="$" + str(round(orders["Asks"][-3][0],2)) + " : " + str(round(orders["Asks"][-3][1],2)))
+        self.asks3.update()
+        
+        #Update Bid Tables
+        self.bids1.config(text="$" + str(round(orders["Bids"][0][0],2)) + " : " + str(round(orders["Bids"][0][1],2)))
+        self.bids1.update()
+        self.bids2.config(text="$" + str(round(orders["Bids"][1][0],2)) + " : " + str(round(orders["Bids"][1][1],2)))
+        self.bids2.update()
+        self.bids3.config(text="$" + str(round(orders["Bids"][2][0],2)) + " : " + str(round(orders["Bids"][2][1],2)))
+        self.bids3.update()
+        
+        return
+        
+    def updatehistory(self):
+        connection = CampBX(self)
+        connection.historical()
+        
+        return
+        
